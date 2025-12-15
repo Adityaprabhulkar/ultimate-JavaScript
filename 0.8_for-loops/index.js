35d757555c1259482848670007c151d7dd8488a7
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-15 22:45:36 +0530

    practice types of Loops , For-Loop, For-in-Loop, For-of-Loop, While-Loop, Do-While-Loop

---
+++ 0.8_for-loops/index.js
@@ -0,0 +1,88 @@
+console.log("I am a tutorial on Loops");
+
+// let a = 1;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a +i);
+    
+// }
+  
+
+// let obj = {
+//     name: "Aditya",
+//     role: "programer",
+//     company: "Google"
+// }
+
+// for (const key in obj) {
+//     console.log(key,obj[key]);
+// }
+
+
+// const obj = {
+//     name: "Rahul",
+//     age: 20,
+//     city: "Delhi"
+// };
+
+// for (const key in obj) {
+//     console.log(key, obj[key]);
+    
+// }
+
+// Q1
+// const student = {
+//     name: "Aditya",
+//     age: 21,
+//     Course: "MCA"
+// }
+
+// for (const key in student) {
+//     console.log(key, student[key]);
+    
+// }
+
+// Q2
+// const marks = {
+//     math: 80,
+//     english: 70,
+//     science: 90
+// };
+
+// for (const key in marks) {
+//     console.log( marks[key]);
+    
+// }
+
+// Q3
+// const phone = {
+//     brand: "samsung",
+//     price: 20000,
+//     color: "Black",
+//     stock: true
+// };
+
+// for(const key in phone) {
+//     // console.log(phone.length);
+//     console.log(Object.keys(phone).length);
+    
+// }
+
+// for(const c of "Aditya"){
+//     console.log(c);
+    
+// }
+
+//   let i = 0;
+//   while (i<6) {
+//     console.log(i)
+//     i++;
+//   }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+
+
