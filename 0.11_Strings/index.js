c9e82d827ac97ddfdad4ce213d27312c3398494b
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-16 17:11:55 +0530

    types to write strings

---
+++ 0.11_Strings/index.js
@@ -0,0 +1,31 @@
+// using double qoutes
+
+let name = "Aditya";
+console.log(name);
+
+// using single qoutes
+ let car = 'TaTa';
+ console.log(car);
+
+//  using template litterals
+
+let man1 = "Akshay";
+let man2 = "raj";
+
+console.log(`${man1} is working with ${man2}`);
+
+// Escape  Sequence Characters
+
+let fruit = "Bana\"na";
+console.log(fruit);
+
+let fruit1 = "Bana\nna";
+console.log(fruit1);
+
+let fruit2 = "Bana\rna";
+console.log(fruit2);
+
+let fruit3 = "Bana\tna";
+console.log(fruit3);
+
+ 
