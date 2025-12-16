4d2cffb26f48a63c706cff0f8fd08cc0306b87d5
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-16 21:01:38 +0530

    Learn Arrays methods pop, push, shift, unshift

---
+++ 0.14_Arrays/index.js
@@ -0,0 +1,21 @@
+ let marks = [91, 83, 63, 56, false, "Not Present"]
+
+//  console.log(marks);
+//  console.log(marks[0]);
+//  console.log(marks[1]);
+//  console.log(marks[2]);
+//  console.log(marks[3]);
+//  console.log(marks[4]);
+//  console.log(marks[5]);
+// console.log(marks[6]); //will be undefined coz index 6 is not present
+// console.log(marks.length);
+// marks[6] = 89
+// marks[0] = 96
+// console.log(typeof(marks));
+
+
+ for(let i = 0;i < marks.length; i++) {
+    console.log(marks[i]);
+    
+ }
+ 
---
+++ 0.15_Arrays_method/index.js
@@ -0,0 +1,26 @@
+// Arrays Methods
+
+let num = [1,2,34,4]
+
+let b = num.toString();
+
+console.log(b);
+let c = num.join("-")
+console.log(c, typeof c);
+
+// let r = num.pop()  // pop return the pop elements
+// console.log(num, r);
+
+// let r = num.push(56)  // pop return the pop elements
+// console.log(num, r);
+
+// let r = num.shift()
+// console.log(r, num); // Removes an element for the start of the 
+
+let r = num.unshift(78)
+console.log(r, num); // add an element for the start of the array
+
+
+
+
+
