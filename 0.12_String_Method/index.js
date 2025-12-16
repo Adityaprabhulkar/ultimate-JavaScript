542887d9a620c368e0097333eebc4c0c2c834e92
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-16 18:03:09 +0530

    write String and research string methods

--- 0.12_String_Method/index.js
+++ 0.12_String_Method/index.js
@@ -1,4 +1,4 @@
-let name = "Aditya"
+// let name = "Aditya"
 // console.log(name.length);
 
 // console.log(name.toUpperCase());
@@ -13,7 +13,7 @@ let name = "Aditya"
 // console.log(friend2.trim());
 // console.log(friend2);
 
-let fr = "Shivika"
+// let fr = "Shivika"
 // console.log(fr[0])
 // console.log(fr[1])
 // console.log(fr[2])
@@ -25,15 +25,54 @@ let fr = "Shivika"
     
 // }
 
-for(const key in fr) {
-    console.log(key, fr[key]);
+// for(const key in fr) {
+//     console.log(key, fr[key]);
     
-}
+// }
 
-for (const char of fr) {
-    console.log(char);
+// for (const char of fr) {
+//     console.log(char);
     
-}
+// }
+
+// const string1 = "A string primitive";
+// const string2 = 'Also a string primitive';
+// const string3 = `Yet another string primitive`;
+
+
+// console.log(string1);
+// console.log(string2);
+// console.log(string3);
+
+// const string4 = new String("A String object");
+
+// console.log(string4);
+
+// let boy = "Ram"
+// console.log(boy.charAt(1));
+
+
+// let text = "HELLO WORLD";
+// // let char = text.charAt(0);
+// let char = text.charCodeAt(0)
+// console.log(char);
+
+
+// let name = "AAditya";
+// // let letter = name.at(2);
+// let letter = name[2]
+// console.log(letter);
+
+
+let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
+// let index = text.lastIndexOf("locate")
+// let index = text.lastIndexOf("John");
+// let index = text.indexOf("locate", 15);
+text.lastIndexOf("locate", 15);
+
+console.log(text);
+
 
 
 
