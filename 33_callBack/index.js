41b265024b8b3f16e76c5e3211115e1493dfa079
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-19 20:43:11 +0530

    update

---
+++ 33_callBack/index.js
@@ -0,0 +1,18 @@
+// Synchrouns programing
+// let a ="Aditya"
+// let b = "raj"
+// let c= "oemga"
+
+// console.log(a,b,c);\
+
+
+// Asynchronus Programing
+
+console.log("start");
+setTimeout(function() {
+    console.log("hello i will print after 3 sec");
+}, 3000)
+
+console.log("end");
+
+
