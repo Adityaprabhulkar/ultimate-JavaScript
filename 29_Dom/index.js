b2f1d7db46a2f45a95946029d6af1a026b5b2a35
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-19 19:43:31 +0530

    update

---
+++ 29_Dom/index.js
@@ -0,0 +1,13 @@
+ let ctitle = document.getElementById('firstCardTitle')
+ ctitle.style.color = "blue"
+
+
+ let ctitles = document.querySelectorAll(".card-title")
+  ctitles[0].style.color = "red"
+  ctitles[1].style.color = "green"
+  ctitles[2].style.color = "blue"
+ console.log(ctitles);
+
+
+ document.querySelector('.this').style.color = "white"
+ document.querySelector('.this').style.backgroundColor = "red"
